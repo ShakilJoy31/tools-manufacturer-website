05e4080bd3b5278c8dcb78e2cb5fd9ff8d7fa751
ShakilJoy31 <joy31a@gmail.com>
2022-05-21 23:07:42 +0600

    Initialize a project with Navbar done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home. </h1>
+
+
+
+            <h1 className='text-4xl text-purple-500 font-bold'>Parts We contain.</h1>
+        </div>
+    );
+};
+
+export default Home;
